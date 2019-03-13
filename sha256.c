76d130bc9e4729e31afe309cc7092735490ae972
Derrick Conway <derick.conway15@gmail.com>
2019-03-13 18:12:03 +0000

    Section 6.1.2 part 1

--- sha256.c
+++ sha256.c
@@ -9,6 +9,14 @@
 
 void sha256();
 
+// see Sections 4.1.2 for defentions
+uint32_t sig0(uint32_t x);
+uint32_t sig1(uint32_t x);
+
+// See Section 3.2 for defintions
+uint32_t rotr(uint32_t n, uint32_t x);
+uint32_t shr(uint32_t n, uint32_t x);
+
 int main(int argc, char *argv[]) {
 
   sha256();
@@ -41,8 +49,43 @@ void sha256(){
   
   // The current message block
   uint32_t M[16];
-
   
+  // for looping
+  int t;
+
+  // from page 22 of the standered W[t] for 0 <= t <= 15.
+  for (t =0; t < 16; t++)
+    W[t] = M[t];
+ 
+  // from page 22 W[t] =....
+  for (t = 16; t < 64; t++)
+    sig_1(W[-2]) + W[t-7] + sig_0(W[t-15]) + W[t-16];
+
+
 
 }
+
+// se Section 3.2 for defintions.
+uint32_t rotr(uint32_t n, uint32_t x);{
+  return (x >> n) | (x << (32 - n);
+
+}
+uint32_t shr(uint32_t n, uint32_t x);{
+  return (x >>n);
+
+}
+
+uint32_t sig0(uint32_t x);{
+  // See Section 3.2 and 4.1.2 for definitions.
+  return (rotr(7, x) ^ rotr(18,x) ^ shr(3, x));
+
+}
+
+uint32_t sig1(uint32_t x);{
+  // See Section 3.2 and 4.1.2 for defintions
+  return (rotr(17, x) ^ rotr(19, x) ^ shr(10, x));
+
+
+}
+
 
