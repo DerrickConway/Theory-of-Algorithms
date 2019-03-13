7215e6630df3efdf4026ebf9b271b5e3d872dc11
Derrick Conway <derick.conway15@gmail.com>
2019-03-13 16:42:23 +0000

    creat sha256

---
+++ sha256.c
@@ -0,0 +1,22 @@
+// Derrick Conway, 2019
+// The Secure Hash Alorithm, 256 bit version
+// Link, https://ws680.nist.gov/publication/get_pdf.cfm?pub_id=910977
+
+// The input output header file
+#include <stdio.h>
+// The use of fixed lenght integers.
+#include <stdint.h>
+
+void sha256();
+
+int main(int argc, char *argv[]) {
+
+  return 0;
+}
+
+viod sha256(){
+
+
+
+}
+
