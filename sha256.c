6f784db476ca88ba81774bfc73c870a5313cee10
Derrick Conway <derick.conway15@gmail.com>
2019-03-13 17:07:35 +0000

    Section 6.2.1

--- sha256.c
+++ sha256.c
@@ -22,11 +22,27 @@ void sha256(){
   uint32_t w[64];
   //Working variables (Section 6.2)
   uint32_t a, b, c, d, e, f, g, h;
-  // The hash value (Section 6.2)
-  uint32_t H[8];
   // Two temporery variables (Section 6.2)
   uint32_t T1, T2;
  
+  // The hash value (Section 6.2)
+  // the values come from (Section 5.3.3)
+  uint32_t H[8] = {
+      0x6a09a667
+    , 0xbb67ae85
+    , 0x3c6ef372
+    , 0xa54ff53a
+    , 0x510e527f
+    , 0x9b05688c
+    , 0x1f83d9ad
+    , 0x5be0cd19
+
+  };
+  
+  // The current message block
+  uint32_t M[16];
+
+  
 
 }
 
