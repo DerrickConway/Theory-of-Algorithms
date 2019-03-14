19f1c58578f9df37ab409d3e5b83a3794f5c9158
Derrick Conway <derick.conway15@gmail.com>
2019-03-14 21:32:48 +0000

    padfile finish

--- padfile.c
+++ padfile.c
@@ -7,6 +7,7 @@ union msgblock {
   uint64_t s[8];
 };
 
+enum status {READ ,PAD0, PAD1, FINISH};
 
 int main(int argc, char *argv[]){
   
@@ -15,11 +16,16 @@ int main(int argc, char *argv[]){
   uint64_t nobytes;
 
   uint64_t nobits = 0;
+
+  enum status S = READ;
   
   FILE* f;
   f = fopen(argv[1], "r");
 
-  while (!feof(f)) {
+  int i;
+
+ // enum status S = READ;
+  while (S == READ) {
     nobytes = fread(M.e, 1, 64, f);
     printf("Read %2llu bytes\n", nobytes);
     nobits = nobits + (nobytes * 8);
@@ -31,9 +37,25 @@ int main(int argc, char *argv[]){
         M.e[nobytes] = 0x00;
       }
       M.s[7] = nobits;
-    }
+      S = FINISH;
+    } else if (nobytes < 64) {
+        S = PAD0;
+        M.e[nobytes] = 0x80;
+        while (nobytes < 64) {
+          nobytes = nobytes + 1;
+          M.e[nobytes] = 0x00;
+      }
+     } else if (feof(f)) {
+        S = PAD1;
+      }  
     // printf("%llu\n", nobytes);
   }
+  if (S == PAD0 || S == PAD1) {
+    for (i = 0; i < 56; i++)
+        M.e[i] = 0x00;
+  }
+  if (S == PAD1)
+    M.e[0] = 0x80;
 
   fclose(f);
 
